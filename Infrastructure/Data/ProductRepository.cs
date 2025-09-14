4800b67c4ff8d653ef14ef5a3e6b89099fae677f
Peter Wei <peterwei@Peters-MacBook-Pro.local>
2025-09-14 10:36:31 +1000

    Implementing the repository methods

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,43 +1,46 @@
 using System.Security.Cryptography;
 using Core.Entities;
 using Core.Interfaces;
+using Humanizer;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
-public class ProductRepository : IProductRepository
+public class ProductRepository(StoreContext context) : IProductRepository
 {
+
     public void AddProduct(Product product)
     {
-        throw new NotImplementedException();
+        context.Products.Add(product);
     }
 
     public void DeleteProduct(Product product)
     {
-        throw new NotImplementedException();
+        context.Products.Remove(product);
     }
 
-    public Task<Product?> GetProductByIdAsync(int id)
+    public async Task<Product?> GetProductByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await context.Products.FindAsync(id);
     }
 
-    public Task<IReadOnlyList<Product>> GetProductsAsync()
+    public async Task<IReadOnlyList<Product>> GetProductsAsync()
     {
-        throw new NotImplementedException();
+        return await context.Products.ToListAsync();
     }
 
     public bool ProductExists(int id)
     {
-        throw new NotImplementedException();
+        return context.Products.Any(x => x.Id == id);
     }
 
-    public Task<bool> SaveAllAsync()
+    public async Task<bool> SaveAChangesAsync()
     {
-        throw new NotImplementedException();
+        return await context.SaveChangesAsync() > 0;
     }
 
-    public Oid UpdateProuct(Product product)
+    public void UpdateProduct(Product product)
     {
-        throw new NotImplementedException();
+        context.Entry(product).State = EntityState.Modified;
     }
 }

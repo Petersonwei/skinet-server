728b1ae2ac63f5a8f2453280306981ed0994750d
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-09-30 20:39:00 +1000

    Remove project.nuget.cache file to clean up unnecessary cache data

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -30,7 +30,7 @@ public class ProductRepository(StoreContext context) : IProductRepository
         return await context.Products.FindAsync(id);
     }
 
-    public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type)
+    public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort)
     {
         var query = context.Products.AsQueryable();
 
@@ -39,6 +39,16 @@ public class ProductRepository(StoreContext context) : IProductRepository
 
         if (!string.IsNullOrWhiteSpace(type))
             query = query.Where(x => x.Type == type);
+
+        if (!string.IsNullOrWhiteSpace(sort))
+        {
+            query = sort switch
+            {
+                "priceAsc" => query.OrderBy(x => x.Price),
+                "priceDesc" => query.OrderByDescending(x => x.Price),
+                _ => query.OrderBy(x => x.Name)
+            };
+        }
 
         return await query.ToListAsync();
     }

204a54f7ba1e0e2343f4f32b3948925a9223e4e6
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-10-11 12:22:47 +1000

    Enhance product filtering by adding sorting capabilities in ProductSpecification and updating related interfaces and evaluators

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using System.Linq.Expressions;
 using Core.Interfaces;
 
@@ -11,6 +12,20 @@ namespace Core.Specifications;
 public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
 {
     protected BaseSpecification() : this(null) { }
-    
+
     public Expression<Func<T, bool>>? Criteria => criteria;
+
+    public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+    public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+    {
+        OrderByDescending = orderByDescExpression;
+    }
 }

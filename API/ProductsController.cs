c376e04bd4e600b26f5d7edd4e6784c21002b89e
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-10-12 11:41:24 +1000

    Add pagination support and count method to GenericRepository; enhance ISpecification with ApplyCriteria

--- API/ProductsController.cs
+++ API/ProductsController.cs
@@ -18,8 +18,11 @@ public class ProductsController(IGenericRepository<Product> repo) : ControllerBa
         var spec = new ProductSpecification(specParams);
 
         var products = await repo.ListAsync(spec);
+        var count = await repo.CountAsync(spec);
 
-        return Ok(products);
+        var pagination = new Pagination<Product>(specParams.PageIndex, specParams.PageSize, count, products);
+
+        return Ok(pagination);
     }
 
     [HttpGet("{id:int}")] // api/products/2

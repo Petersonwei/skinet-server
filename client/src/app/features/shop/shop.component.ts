25078ff4f5f9fe36e7e623a92b1b8074c4e68145
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-11-25 15:54:08 +1000

    #0076 feat: refactor Shop component to use ProductItemComponent for product display

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MatCard } from '@angular/material/card';
 import { Product } from '../../shared/models/product';
 import { Pagination } from '../../shared/models/pagination';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @Component({
   selector: 'app-shop',
-  imports: [MatCard],
+  imports: [ProductItemComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })

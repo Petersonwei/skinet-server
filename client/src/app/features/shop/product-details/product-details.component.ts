397094ae5af381fd6c236dec7084930ff88b49d8
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-11-29 14:27:16 +1000

    #0090 feat: enhance product details component with image, add to cart button, and quantity input

--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -2,10 +2,16 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ShopService } from '../../../shared/services/shop.service';
 import { Product } from '../../../shared/models/product';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CurrencyPipe, MatButton, MatIcon, MatFormField, MatLabel, MatInput, MatDivider],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
@@ -24,10 +30,10 @@ export class ProductDetailsComponent implements OnInit {
     if (!id) return;
 
     this.shopService.getProduct(+id).subscribe({
-      next: (product) => {
+      next: (product: Product) => {
         this.product = product;
       },
-      error: (error) => {
+      error: (error: any) => {
         console.log(error);
       }
     });

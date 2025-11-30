966938ce1f8dd7ea815bb149d5916de2f2dd2466
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-01 07:01:16 +1000

    #0099 feat: redesign NotFound component; update layout and styles for better user experience

--- client/src/app/shared/components/not-found/not-found.component.ts
+++ client/src/app/shared/components/not-found/not-found.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [MatIcon, MatButton, RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })

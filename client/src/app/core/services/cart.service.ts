8973f90a26033b561915020ef81ee69e222a2765
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-07 12:43:08 +1000

    #0115 feat: enhance CartService with addItemToCart and cart management methods

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,8 +1,56 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Cart, CartItem, CartType } from '../../shared/models/cart';
+import { Product } from '../../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  
+  private cart = signal<CartType | null>(null);
+
+  addItemToCart(item: CartItem | Product, quantity = 1) {
+    const cart = this.cart() ?? this.createCart();
+
+    if (this.isProduct(item)) {
+      item = this.mapProductToCartItem(item);
+    }
+
+    cart.items = this.addOrUpdateItem(cart.items, item, quantity);
+    this.cart.set(cart);
+  }
+
+  private createCart(): Cart {
+    const cart = new Cart();
+    localStorage.setItem('cart_id', cart.id);
+    return cart;
+  }
+
+  private isProduct(item: CartItem | Product): item is Product {
+    return (item as Product).id !== undefined;
+  }
+
+  private mapProductToCartItem(item: Product): CartItem {
+    return {
+      productId: item.id,
+      productName: item.name,
+      price: item.price,
+      quantity: 0,
+      pictureUrl: item.pictureUrl,
+      brand: item.brand,
+      type: item.type
+    };
+  }
+
+  private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
+    const index = items.findIndex(x => x.productId === item.productId);
+
+    if (index === -1) {
+      item.quantity = quantity;
+      items.push(item);
+    } else {
+      items[index].quantity += quantity;
+    }
+
+    return items;
+  }
 }

6e3f6d6dd52a91a2c3be0057f86e138c40135c6d
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-07 22:09:13 +1000

    #0126 feat: enhance product details component with cart quantity management and update functionality

--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -8,18 +8,23 @@ import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatDivider } from '@angular/material/divider';
+import { FormsModule } from '@angular/forms';
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CurrencyPipe, MatButton, MatIcon, MatFormField, MatLabel, MatInput, MatDivider],
+  imports: [CurrencyPipe, MatButton, MatIcon, MatFormField, MatLabel, MatInput, MatDivider, FormsModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
 export class ProductDetailsComponent implements OnInit {
   product?: Product;
+  quantityInCart = 0;
+  quantity = 1;
 
   private shopService = inject(ShopService);
   private activatedRoute = inject(ActivatedRoute);
+  private cartService = inject(CartService);
 
   ngOnInit(): void {
     this.loadProduct();
@@ -32,10 +37,34 @@ export class ProductDetailsComponent implements OnInit {
     this.shopService.getProduct(+id).subscribe({
       next: (product: Product) => {
         this.product = product;
+        this.updateQuantityInCart();
       },
       error: (error: any) => {
         console.log(error);
       }
     });
+  }
+
+  updateQuantityInCart() {
+    this.quantityInCart = this.cartService.cart()?.items?.find(x => x.productId === this.product?.id)?.quantity ?? 0;
+    this.quantity = this.quantityInCart || 1;
+  }
+
+  getButtonText() {
+    return this.quantityInCart > 0 ? 'Update Cart' : 'Add to Cart';
+  }
+
+  updateCart() {
+    if (!this.product) return;
+
+    if (this.quantity > this.quantityInCart) {
+      const itemsToAdd = this.quantity - this.quantityInCart;
+      this.quantityInCart += itemsToAdd;
+      this.cartService.addItemToCart(this.product, itemsToAdd);
+    } else {
+      const itemsToRemove = this.quantityInCart - this.quantity;
+      this.quantityInCart -= itemsToRemove;
+      this.cartService.removeItemFromCart(this.product.id, itemsToRemove);
+    }
   }
 }

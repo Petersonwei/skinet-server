1159b7426efc19e439bbcf3c5ba0f6237c4da440
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-07 20:01:29 +1000

    #0122 feat: enhance OrderSummaryComponent with voucher code input and improve layout

--- client/src/app/shared/components/order-summary/order-summary.component.ts
+++ client/src/app/shared/components/order-summary/order-summary.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-order-summary',
-  imports: [],
+  imports: [MatButton, RouterModule, MatFormField, MatLabel, MatInput],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss'
 })

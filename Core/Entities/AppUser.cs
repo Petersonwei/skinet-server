2ac2a06cdcbcf5265992cf1fc796811897b52782
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-08 18:06:39 +1000

    #0130 feat: integrate identity management with AppUser entity and update services for authorization

---
+++ Core/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Core.Entities;
+
+public class AppUser : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}

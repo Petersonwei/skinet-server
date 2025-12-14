b454b2baf10308c3659e80fa3a257d83f85abd1b
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-14 20:37:32 +1000

    #0153 feat: Client-Side Route Guards (AuthGuard)

--- client/src/app/features/account/login/login.component.ts
+++ client/src/app/features/account/login/login.component.ts
@@ -5,7 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { AccountService } from '../../../core/services/account.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -23,18 +23,28 @@ export class LoginComponent {
   private fb = inject(FormBuilder);
   private accountService = inject(AccountService);
   private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+
+  returnUrl = '/shop';
 
   loginForm = this.fb.group({
     email: [''],
     password: ['']
   });
 
+  constructor() {
+    const url = this.activatedRoute.snapshot.queryParams['returnUrl'];
+    if (url) {
+      this.returnUrl = url;
+    }
+  }
+
   onSubmit() {
     this.accountService.login(this.loginForm.value).subscribe({
       next: () => {
         this.accountService.getUserInfo().subscribe({
           next: () => {
-            this.router.navigateByUrl('/shop');
+            this.router.navigateByUrl(this.returnUrl);
           }
         });
       }

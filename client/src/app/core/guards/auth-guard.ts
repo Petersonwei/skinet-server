3d77cda0dc5f94669e500a035588d6d3a692802e
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-14 20:47:31 +1000

    #0154 Fixing Auth Guard Timing Issues (Signals vs. Observables)

--- client/src/app/core/guards/auth-guard.ts
+++ client/src/app/core/guards/auth-guard.ts
@@ -1,17 +1,27 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AccountService } from '../services/account.service';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const accountService = inject(AccountService);
   const router = inject(Router);
 
   if (accountService.currentUser()) {
-    return true;
+    return of(true);
   } else {
-    router.navigate(['/account/login'], {
-      queryParams: { returnUrl: state.url }
-    });
-    return false;
+    return accountService.getAuthState().pipe(
+      map(auth => {
+        if (auth.isAuthenticated) {
+          return true;
+        } else {
+          router.navigate(['/account/login'], {
+            queryParams: { returnUrl: state.url }
+          });
+          return false;
+        }
+      })
+    );
   }
 };

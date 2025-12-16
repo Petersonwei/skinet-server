897f936b41aa971346639489b9bb15c0df5f2410
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-16 22:12:01 +1000

    #0185 Creating the Order Entity

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities.OrderAggregate;
+
+public class Order : BaseEntity
+{
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    public required string BuyerEmail { get; set; }
+
+    public ShippingAddress ShippingAddress { get; set; } = null!;
+
+    public DeliveryMethod DeliveryMethod { get; set; } = null!;
+
+    public PaymentSummary PaymentSummary { get; set; } = null!;
+
+    public IReadOnlyList<OrderItem> OrderItems { get; set; } = [];
+
+    public decimal Subtotal { get; set; }
+
+    public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+    [Required]
+    public required string PaymentIntentId { get; set; }
+}

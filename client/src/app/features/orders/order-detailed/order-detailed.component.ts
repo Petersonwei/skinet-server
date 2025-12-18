7d52552b9d8dbc8e97fcb22904e1abec448687df
Peter Wei <140482566+Petersonwei@users.noreply.github.com>
2025-12-18 23:11:39 +1000

    #0201 Order Detailed Component

--- client/src/app/features/orders/order-detailed/order-detailed.component.ts
+++ client/src/app/features/orders/order-detailed/order-detailed.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DatePipe, CurrencyPipe } from '@angular/common';
+import { OrderService } from '../../../core/services/order.service';
+import { Order } from '../../../shared/models/order';
 
 @Component({
   selector: 'app-order-detailed',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, DatePipe, CurrencyPipe],
   templateUrl: './order-detailed.component.html',
   styleUrl: './order-detailed.component.scss'
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+  private orderService = inject(OrderService);
+  private activatedRoute = inject(ActivatedRoute);
+  order?: Order;
 
+  ngOnInit() {
+    this.loadOrder();
+  }
+
+  loadOrder() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) return;
+
+    this.orderService.getOrderDetails(+id).subscribe({
+      next: (order) => {
+        this.order = order;
+      }
+    });
+  }
 }
